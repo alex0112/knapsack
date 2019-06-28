419bc5ed32eb00af9153148788a4e4ecce0ce2cc
alex0112 <alarsen0112@gmail.com>
2019-06-27 22:00:29 -0600

    Boilerplate

---
+++ lib/knapsack.ex
@@ -0,0 +1,18 @@
+defmodule Knapsack do
+  @moduledoc """
+  Documentation for Knapsack.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Knapsack.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
